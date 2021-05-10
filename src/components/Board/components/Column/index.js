f6db2616996aad0e9b2e655ecc795a96ada282e6
vin1891 <40721849+vin1891@users.noreply.github.com>
2021-05-10 11:59:06 -0300

    adjusments

--- src/components/Board/components/Column/index.js
+++ src/components/Board/components/Column/index.js
@@ -134,11 +134,13 @@ function Column({
               </Droppable>
             ) : (
               <DroppableColumn droppableId={String(children.id)}>
-                {children.cards.length ? (
-                  children.cards.map((card, index) => getCard(card, index))
-                ) : (
-                  <div className='react-kanban-card-skeleton' />
-                )}
+                <div className='react-kanban-column-cards-wrapper'>
+                  {children.cards.length ? (
+                    children.cards.map((card, index) => getCard(card, index))
+                  ) : (
+                    <div className='react-kanban-card-skeleton' />
+                  )}
+                </div>
               </DroppableColumn>
             )}
           </div>
